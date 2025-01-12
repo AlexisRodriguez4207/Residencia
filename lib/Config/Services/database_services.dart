7f9d67bf4064a5eb9c1904fd63e16495e5713393
Xero4207 <carlos.rodriguez201@tectijuana.edu.mx>
2025-01-11 18:20:28 -0800

    Agregando la creación base de las Notas

--- lib/Config/Services/database_services.dart
+++ lib/Config/Services/database_services.dart
@@ -1,8 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class DatabaseServices {
+  final auth = FirebaseAuth.instance;
+
   final CollectionReference userCollection =
       FirebaseFirestore.instance.collection('user');
+
+  final CollectionReference notesCollection =
+      FirebaseFirestore.instance.collection('notes');
 
   Future<bool> addUser(
       String name, String lastname, int age, String email) async {
@@ -18,5 +24,28 @@ class DatabaseServices {
     } catch (e) {
       return false;
     }
+  }
+
+  addNote(
+    String note,
+  ) {
+    notesCollection.add({
+      'uidcreador': auth.currentUser!.uid,
+      'text': note,
+      'date': Timestamp.now()
+    });
+  }
+
+  notelist() {
+    // Esto serviría para mostrar todas las notas del usuario
+    return notesCollection
+        .where('uidcreador', isEqualTo: auth.currentUser!.uid)
+        .snapshots();
+
+    // Esto serviriá para limitar la cantidad de notas que se muestran
+    //return notesCollection.limit(10).snapshots();
+
+    // Esto serviría para ordenar las notas por fecha
+    //return notesCollection.orderBy('date', descending: true).snapshots();
   }
 }
--- lib/Screen/Modules/notes.dart
+++ lib/Screen/Modules/notes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:residencia_v2/Config/Preferences/shared_preferences.dart';
+import 'package:residencia_v2/Config/Services/database_services.dart';
 import 'package:residencia_v2/Screen/Custom/custom_form_text_field.dart';
 import 'package:sizer/sizer.dart';
 
@@ -12,6 +14,7 @@ class Notes extends StatefulWidget {
 }
 
 class _NotesState extends State<Notes> {
+  final _formKey = GlobalKey<FormBuilderState>();
   var prefs = UserPreferences();
 
   @override
@@ -31,19 +34,7 @@ class _NotesState extends State<Notes> {
           heroTag: 'floating2',
           onPressed: () {
             // Agregar Notas
-            showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  content: FormBuilder(
-                    child: CustomFormTextField(
-                        nombre: 'Notas',
-                        hint: 'Ingresa un texto',
-                        label: 'Notas'),
-                  ),
-                );
-              },
-            );
+            showdialogadd(context);
           },
           child: const Icon(Icons.add),
         )
@@ -52,6 +43,102 @@ class _NotesState extends State<Notes> {
         centerTitle: true,
         title: const Text('Notas'),
       ),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            height: 80.h,
+            width: 70.w,
+            child: StreamBuilder(
+              stream: DatabaseServices().notelist(),
+              builder: (context, AsyncSnapshot snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: const CircularProgressIndicator());
+                } else {
+                  var notes = snapshot.data.docs;
+
+                  return ListView.builder(
+                    itemCount: notes.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.pinkAccent,
+                        ),
+                        height: 6.h,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              notes[index]['text'],
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.edit)),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.delete_outline)),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> showdialogadd(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: FormBuilder(
+            key: _formKey,
+            child: CustomFormTextField(
+                nombre: 'notas',
+                hint: 'Ingresa un texto',
+                label: 'Notas',
+                validator: FormBuilderValidators.compose([
+                  FormBuilderValidators.minLength(1),
+                ])),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                //Cerrar dialog
+                Navigator.pop(context);
+              },
+              child: Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () async {
+                //Guardar Notas
+                _formKey.currentState?.save();
+                if (_formKey.currentState?.validate() == true) {
+                  final nota = _formKey.currentState!.value;
+                  await DatabaseServices().addNote(nota['notas']);
+                  if (context.mounted) {
+                    Navigator.pop(context);
+                    _formKey.currentState?.reset();
+                  }
+                }
+              },
+              child: const Text('Guardar'),
+            )
+          ],
+        );
+      },
     );
   }
 }
