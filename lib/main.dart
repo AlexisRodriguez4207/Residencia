903328e486a4fcc4bac383a1de022c735202e3ee
Xero4207 <carlos.rodriguez201@tectijuana.edu.mx>
2025-01-15 23:39:07 -0800

    Modificando Inicio de App

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
           //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           //   // useMaterial3: true,
           // ),
-          initialRoute: '/maps',
+          initialRoute: 'main',
           routes: routes,
         );
       },
